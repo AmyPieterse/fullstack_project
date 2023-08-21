530e67af29ef9285a42fc5b62b3558b749fd6f37
Taflnne15 <taflynnepetersen@gmail.com>
2023-08-21 16:33:33 +0200

    almostDone

---
+++ orbitoptics_gui/src/components/FooterComp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="Footer">
+        <div class="container text-center">
+  <div class="row align-items-center">
+    <div class="col">
+  <h4>OrbitOptics</h4>
+    </div>
+    <div class="col">
+   <h4>Navigate our pages:</h4>
+
+   <h6>Home<br>
+About<br>Products<br>Admin<br>Reach Us</h6>
+    </div>
+    <div class="col">
+       <a href="https://www.instagram.com/"><img src="https://i.postimg.cc/Pf09sfd3/instagram-removebg-preview.png" alt=""></a>
+       <a href="https://www.facebook.com/"><img src="https://i.postimg.cc/2yYWhB49/Screenshot-2023-08-21-100537-removebg-preview.png" alt=""></a>
+<h6>Follow our social media</h6>
+    </div>
+  </div>
+</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+h4{
+    color: white;
+}
+h6{
+    color: white;
+}
+img{
+    width: 50px;
+}
+</style>
--- orbitoptics_gui/src/views/HomeView.vue
+++ orbitoptics_gui/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="img-div">
+      <img id="image_2" src="https://i.postimg.cc/R0LrTkfS/homeview-img-1.jpg" alt="homeview">
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
+
+<style scoped>
+.img-div{
+  width: 100%;
+  display:flex;
+  justify-content: center;
+}
+#image_2 {
+  width: 80%;
+  height: 110vh;
+}
+
+</style>
