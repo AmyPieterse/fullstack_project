16eb73b4dd067210bbcdd27a2d6f208fa41d7995
Taflnne15 <taflynnepetersen@gmail.com>
2023-08-23 11:20:52 +0200

    did alot

--- orbitoptics_gui/src/views/AdminView.vue
+++ orbitoptics_gui/src/views/AdminView.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
+    <div class="Admin">
+        <table>
+        <tr>
+          <th>Name </th>
+          <th>Price</th>
+          <th>Image</th>
+          <th>edit</th>
+          <th>delete</th>
+        </tr>
+        
+        <tbody id="whereTheyDisplay">
+        </tbody>
+      </table>
+     
 
+`
+    <tr class="tr">
+    <td></td>
+    <td></td>
+    <td></td>
+    <td><img src="${item.products_image }" alt="" width="50px" heigth="50px"></td>
+    <td><button class="edit-btn">Edit</button></td>
+    <td><button class="del-btn" onclick="deleteProduct() ">Delete</button></td>
+    </tr>
+    `
+
+      
     </div>
 </template>
 
@@ -11,5 +36,8 @@
 </script>
 
 <style scoped>
+
+
+
 
 </style>
--- orbitoptics_gui/src/views/ProductView.vue
+++ orbitoptics_gui/src/views/ProductView.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-
+  <div class="Product">
+    <div class="card" style="width: 18rem" v-for="product in products" :key="product">
+      <img :src="product.prodURL" class="card-img-top" :alt="product.prodName" />
+      <div class="card-body">
+        <h5 class="card-title">{{ product.prodName }}</h5>
+        <p class="card-text">
+          {{ product.categoryy }}
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchProducts");
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
